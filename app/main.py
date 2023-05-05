8b064d9313f6cb3e9de706cbaf79f6319d17a23a
Luis David <luisdavidg.98@gmail.com>
2023-05-05 14:41:52 -0500

    Adding app folder with files

---
+++ app/main.py
@@ -0,0 +1,26 @@
+import utils
+import read_csv
+import charts
+
+
+def run():
+
+  data = read_csv.read_csv('data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America', data))
+
+  labels, values = utils.percentage_population(data)
+  charts.generate_pie_chart(labels, values)
+
+  country = input("Type Country => ")
+  result = utils.population_by_country(data, country)
+  
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
+  
+
+
+if __name__ == '__main__':
+  run()
